2c7db5b5589d19194d3d74ec7d3a0afcaa028ca9
mogfull7777 <anjunyoung94@gmail.com>
2023-05-10 00:03:55 +0900

    recap #4 (get Detail)

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
-import {useParams} from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
 import axios from "axios";
+import {Card, Col, Container, Image, ListGroup, Row, Form, Button} from "react-bootstrap";
 
 const Detail = () => {
 
@@ -10,6 +11,7 @@ const Detail = () => {
     // useParams() => api를 들고 오는 훅. 같은 계열로 useStats, useEffact 가 있다.
 
     const [product, setProduct] = useState({})
+    const [qty, setQty] = useState(0)
 
     const getProduct = async () => {
 
@@ -39,9 +41,106 @@ const Detail = () => {
     }, [])
 
     return (
-        <div>
-            <h1>{product.name}</h1>
-        </div>
+        <Container>
+            <Link className={"btn btn-light my-3"} to={"/"}>
+                Go Back
+            </Link>
+
+            <>
+                <Row>
+                    {/* 줄 */}
+                    <Col md={4}>
+                        {/*칼럼 (칸)*/}
+                        <Image src={product.image} alt={product.name} fluid />
+                    </Col>
+                    <Col md={4}>
+                        <ListGroup variant={"flush"}>
+
+                            <ListGroup.Item>
+                                <h3>{product.name}</h3>
+                            </ListGroup.Item>
+
+                            <ListGroup.Item>
+                                <h3>{product.rating}</h3>
+                            </ListGroup.Item>
+
+                            <ListGroup.Item>
+                                Price : ${product.price}
+                            </ListGroup.Item>
+
+                            <ListGroup.Item>
+                                Description : {product.description}
+                            </ListGroup.Item>
+
+                        </ListGroup>
+                    </Col>
+
+                    <Col md={4}>
+                        <Card>
+                            <ListGroup variant={"flush"}>
+                                <ListGroup.Item>
+                                    <Row>
+                                        <Col>price:</Col>
+                                        <Col>
+                                            <strong>${product.price}</strong>
+                                        </Col>
+                                    </Row>
+                                </ListGroup.Item>
+                                <ListGroup.Item>
+                                    <Row>
+                                        <Col>Status</Col>
+                                        <Col>
+                                            {product.countInStock > 0 ? "In Stock" : "Out of Stock"}
+                                        </Col>
+                                    </Row>
+                                </ListGroup.Item>
+                                {product.countInStock > 0 && (
+                                    <ListGroup.Item>
+                                        <Row>
+                                            <Col>
+                                                QTY
+                                            </Col>
+                                            <Col>
+                                                <Form.Control
+                                                    as={"select"}
+                                                    value={qty}
+                                                    onChange={e => setQty(e.target.value)}
+                                                >
+                                                    {[...Array(product.countInStock).keys()].map(
+                                                        (x) => (
+                                                            <option key = {x + 1} value={x + 1}>
+                                                                {x+1}
+                                                            </option>
+                                                        )
+                                                    )}
+
+                                                </Form.Control>
+                                            </Col>
+                                        </Row>
+                                    </ListGroup.Item>
+                                )}
+                                <ListGroup.Item>
+                                    <Button
+                                        className={"btn-block"}
+                                        type={"button"}
+                                        disabled={product.countInStock === 0}
+                                    >
+                                        Add to Cart
+                                    </Button>
+                                </ListGroup.Item>
+                            </ListGroup>
+                        </Card>
+                    </Col>
+
+                {/* 4번 이상! 하루 2시간 이상!   */}
+                {/*  전에 했던 프로젝트 복습. 30개 40개  */}
+                {/*  제품 가짓수도 늘리고 카테고리 다 다르게 늘리기 (개당 3에서 4개) */}
+
+
+                </Row>
+            </>
+
+        </Container>
     );
 };
 
